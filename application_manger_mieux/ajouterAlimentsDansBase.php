166f2690d0d1a795fbc0094bb20293318f67652e
GuilhemMaire <maire.guilhem@gmail.com>
2024-10-21 17:28:36 +0200

    php pour récup les données alimentaires

--- application_manger_mieux/ajouterAlimentsDansBase.php
+++ application_manger_mieux/ajouterAlimentsDansBase.php
@@ -1,4 +1,5 @@
 <?php
+    require("init_pdo.php");
     $nomFichierAliments = "aliments.xlsx";
     $matrice = array();
     $fileAliments = fopen($nomFichierAliments, "r");
@@ -20,6 +21,27 @@
     }
     fclose($fileAliments);
 
-    foreach($matrice as index=>aliment){
-        
+    foreach($matrice[0] as $index=>$value){
+        if($index > 1){
+            $request = "INSERT INTO `caracteristiques_de_sante` (désignation) VALUES ($value);"
+            $reponse = $db->query($requete);
+        }
+    }
+
+    foreach($matrice as $index=>$ligne){
+        if($index !== 0){
+
+            foreach($ligne as $indexLigne=>$value){
+                $id_aliment = "";
+                if($indexLigne === 0){
+                    $ajouterAliment = "INSERT INTO `aliments` (nom) VALUES ($value);";
+                    $id_aliment = $pdo->query("SELECT Aliment.id_aliment FROM Aliment WHERE nom=$value");
+                }
+                else{
+                    $request = "INSERT INTO `a_comme_caracteristiques` (Id_aliment,Id_caracteristique,Pourcentage) VALUES($id_aliment,(SELECT caracteristiques_de_sante.id_caracteristique FROM caracteristiques_de_sante WHERE designation=$matrice[0][$indexLigne),$value)";
+                }
+            }
+
+            $request = "INSERT INTO `a_comme_caracteristiques` (Id_aliment,Id_caracteristique,Pourcentage) VALUES()";
+        }
     }
